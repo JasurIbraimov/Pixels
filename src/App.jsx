d077a58cc40ecd2144e961b52a9a801cbf46cfbb
Jasur <ja@iMac-Jasur.local>
2021-01-24 13:48:54 +0600

    starting project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+// Base
+import React from 'react'
+
+// Styles
+import styles from './App.module.css'
+const App = () => {
+	return (
+		<div className={styles.app}>
+			<div className={styles.container}>
+				<header className={styles.header}>
+					<h2>
+						Year
+						<br />
+						in
+					</h2>
+					<h1></h1>
+				</header>
+
+			</div>
+		</div>
+	)
+}
+
+export default App
---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,14 @@
+// Base 
+import React from 'react'
+
+// Styles
+import styles from './Calendar.module.css'
+const Calendar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Calendar
